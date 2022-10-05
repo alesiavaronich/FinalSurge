2abed3eeb717e09a8f9b4b72adefe4ed93cf05e0
Alesia <alesiavaronich@gmail.com>
2022-10-05 17:21:36 +0300

    Created project structure

--- src/test/java/constants/Credentials.java
+++
@@ -1,6 +0,0 @@
-package constants;
-
-public class Credentials {
-    public static final String USERNAME = "gaseicreiquetti-3552@yopmail.com";
-    public static final String PASSWORD = "#$%-Jojokap9871";
-}
