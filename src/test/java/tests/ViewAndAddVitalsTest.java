5aa80139cf45940ec64283157e1f13d40756a513
Alesia <alesiavaronich@gmail.com>
2022-10-21 21:33:20 +0300

    added new pages and tests

--- src/test/java/tests/ViewAndAddVitalsTest.java
+++ src/test/java/tests/ViewAndAddVitalsTest.java
@@ -7,7 +7,7 @@ import pages.LoginPage;
 public class ViewAndAddVitalsTest extends BaseTest{
 
     @Test
-    public void customizeActivityTypesTest() {
+    public void accessDailyVitalsPageTest() {
         LoginPage loginPage = new LoginPage(driver);
         boolean isBreadcrumbDisplayed = loginPage.openLoginPage()
                 .inputLoginName()
@@ -18,4 +18,6 @@ public class ViewAndAddVitalsTest extends BaseTest{
                 .isDailyVitalsBreadcrumbVisible();
         Assert.assertTrue(isBreadcrumbDisplayed, "Breadcrumb is not visible.");
     }
+
+
 }
