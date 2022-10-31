3a87054061dda4c4eaa9ccbb98a7d67437a283e0
Alesia <alesiavaronich@gmail.com>
2022-10-31 19:22:42 +0300

    refactored models

---
+++ src/test/java/constants/Credentials.java
@@ -0,0 +1,7 @@
+package constants;
+
+public class Credentials {
+
+    public static final String LOGIN = "gaseicreiquetti-3552@yopmail.com";
+    public static final String PASSWORD = "@*Kooku928";
+}
